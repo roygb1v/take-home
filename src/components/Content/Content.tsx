38d81b6a6ae738c4d26abdf6dabf29a0f8a67544
Roy <cjhroy@outlook.com>
2024-09-09 17:46:36 +0800

    add padding for mobile devices

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Title, Text } from "@mantine/core";
+import { Title, Text, TextProps } from "@mantine/core";
 
 interface ContentProps {
   title?: React.ReactNode;
@@ -17,18 +17,31 @@ export default function Content({
   description = "Be the first to know when we launch.",
 }: ContentProps) {
   return (
-    <Container direction="column" $alignItems="center" $gap="1rem">
+    <Container
+      direction="column"
+      $alignItems="center"
+      $gap="1rem"
+      $mobilePadding="0rem 0.5rem"
+    >
       <CenteredTitle order={1} size="h1">
         {title}
       </CenteredTitle>
-      <Text size="xl" c="dimmed">
+      <CenteredText size="xl" c="dimmed">
         {description}
-      </Text>
+      </CenteredText>
     </Container>
   );
 }
 
 const CenteredTitle = styled(Title)`
+  text-align: center;
+`;
+
+interface CenteredTextProps extends TextProps {
+  children: React.ReactNode;
+}
+
+const CenteredText = styled(Text)<CenteredTextProps>`
   text-align: center;
 `;
 
@@ -38,6 +51,7 @@ interface ContainerProps {
   $alignItems?: string;
   justifyContent?: string;
   $gap?: string;
+  $mobilePadding?: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -47,4 +61,8 @@ const Container = styled.div<ContainerProps>`
   justify-content: ${(props) => props.justifyContent};
   gap: ${(props) => props.$gap};
   margin-bottom: ${(props) => props.$marginBottom};
+
+  @media (max-width: 768px) {
+    padding: ${(props) => props.$mobilePadding || "0rem"};
+  }
 `;
